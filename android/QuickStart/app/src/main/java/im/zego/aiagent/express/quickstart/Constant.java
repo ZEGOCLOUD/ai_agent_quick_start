f0a6999922fb7a997a27b90d54ac5209274e5242
alan <alanliu@zego.im>
2025-05-22 15:22:56 +0800

    更新注释

--- android/QuickStart/app/src/main/java/im/zego/aiagent/express/quickstart/Constant.java
+++ android/QuickStart/app/src/main/java/im/zego/aiagent/express/quickstart/Constant.java
@@ -2,13 +2,14 @@ package im.zego.aiagent.express.quickstart;
 
 public class Constant {
 
-    public static final long appId = ; // 你部署的业务后台用到的参数
-    public static final String user_id = "user_id_1";    // 你部署的业务后台用到的参数
-    public static final String userName = "user_android";
-    public static final String user_stream_id = "user_stream_id_1";  // 你部署的业务后台用到的参数
-    public static final String room_id = "room_id_1";     // 你部署的业务后台用到的参数
-    public static final String agent_name = "AI Agent Example 1";  // 你部署的业务后台用到的参数
-    public static final String agent_stream_id = "agent_stream_id_1";  // 你部署的业务后台用到的参数
-    public static final String agent_zim_uid = "agent_user_id_1";  // 你部署的业务后台用到的参数
-    public static final String BASE_URL = ; // 你部署的业务后台地址
+    public static final long appId = ;  // 你部署的业务后台用到的参数
+    public static final String user_id = ;  // 你部署的业务后台用到的参数
+    public static final String userName = ;  // 你部署的业务后台用到的参数
+    public static final String user_stream_id = ;  // 你部署的业务后台用到的参数
+    public static final String room_id = ;  // 你部署的业务后台用到的参数
+    public static final String agent_name = ;  // 你部署的业务后台用到的参数
+    public static final String agent_stream_id = ;  // 你部署的业务后台用到的参数
+    public static final String agent_zim_uid = ;  // 你部署的业务后台用到的参数
+    public static final String agent_zim_robotid = ;  // 你部署的业务后台用到的参数
+    public static final String BASE_URL = ;  // 你部署的业务后台地址
 }
