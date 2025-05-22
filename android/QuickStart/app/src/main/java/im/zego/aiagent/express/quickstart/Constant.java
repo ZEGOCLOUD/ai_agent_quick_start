d9fea103e241ff8e2520eea5c6e7fa028b3bcc9f
alan <alanliu@zego.im>
2025-05-22 15:12:01 +0800

    更新quick-start android 代码

---
+++ android/QuickStart/app/src/main/java/im/zego/aiagent/express/quickstart/Constant.java
@@ -0,0 +1,14 @@
+package im.zego.aiagent.express.quickstart;
+
+public class Constant {
+
+    public static final long appId = ;
+    public static final String user_id = "user_id_1";    // 你部署的业务后台用到的参数
+    public static final String userName = "user_android";
+    public static final String user_stream_id = "user_stream_id_1";  // 你部署的业务后台用到的参数
+    public static final String room_id = "room_id_1";     // 你部署的业务后台用到的参数
+    public static final String agent_name = "AI Agent Example 1";  // 你部署的业务后台用到的参数
+    public static final String agent_stream_id = "agent_stream_id_1";  // 你部署的业务后台用到的参数
+    public static final String agent_zim_uid = "agent_user_id_1";  // 你部署的业务后台用到的参数
+    public static final String BASE_URL = ; // 你部署的业务后台地址
+}
